8ce1d95c66d060815a98357b654c477db92c299d
ZakarBalint <szt2zakbal@vasvari.org>
2023-09-14 11:33:40 +0200

    Create AverageOfInput.java

---
+++ AverageOfInput.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class AverageOfInput
+{
+    public static void main(String[] args) 
+    {
+        Scanner be = new Scanner(System.in);
+
+        int sum = 0;
+        double avg = 0;
+
+        for(int i = 0; i < 5; i++)
+        {
+            System.out.print("Please enter a number: ");
+            sum += be.nextInt();
+        }
+
+        avg = Double.valueOf(sum / 5);
+
+        System.out.println("Sum: " + sum + " Avg: " + avg);
+    }
+}
