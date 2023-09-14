de0bc9ec214fed521828f2d945a62da0deb0c786
ZakarBalint <szt2zakbal@vasvari.org>
2023-09-14 13:00:26 +0200

    Create DrawTriangle.java

---
+++ DrawTriangle.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class DrawTriangle {
+    public static void main(String[] args) {
+     
+        Scanner be = new Scanner(System.in);
+
+        System.out.print("Please enter the triangle height:");
+
+        int height = be.nextInt();
+
+        for(int i = 1; i <= height; i++)
+        {
+            for(int y = 1; y <= i; y++)
+            {
+                System.out.print('*');
+            }
+
+            System.out.println();
+        }
+    }
+}
