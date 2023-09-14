632b39cb4994052fdfbc8bf94bc418aa2843172b
ZakarBalint <szt2zakbal@vasvari.org>
2023-09-14 13:24:03 +0200

    Create CodingHours.java

---
+++ CodingHours.java
@@ -0,0 +1,9 @@
+public class CodingHours {
+    public static void main(String[] args) {
+        double coding_h = 6*5*17;
+        double work_h = 17*52;
+
+        System.out.println("Coding hours: " + coding_h);
+        System.out.println("Percentage of the coding hours: " + (coding_h / work_h) * 100);
+    }
+}
