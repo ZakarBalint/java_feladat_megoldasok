ce36fa4072cebd9b0a29833468cda9ecd50244dd
ZakarBalint <szt2zakbal@vasvari.org>
2023-09-18 12:22:59 +0200

    Create BasicInfo.java

---
+++ BasicInfo.java
@@ -0,0 +1,13 @@
+public class BasicInfo {
+    public static void main(String[] args) {
+        String name = "Zakar Bálint";
+        int age = 18;
+        double height = 1.76;
+        boolean married = false;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Married: " + married);
+    }
+}
