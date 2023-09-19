8d2e376e60ebd0d5f18ab1446816915cee12c251
ZakarBalint <szt2zakbal@vasvari.org>
2023-09-19 13:26:37 +0200

    a

---
+++ DrawPyramid.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class DrawPyramid {
+    public static void main(String[] args) {
+        
+        System.out.print("Please enter the pyramid height: ");
+
+        Scanner be = new Scanner(System.in);
+
+        int n = be.nextInt();
+
+        be.close();
+                  
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = n - i; j > 1; j--)
+            {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j <= i; j++ )
+            {
+                System.out.print("*");
+            }
+  
+            System.out.println();
+        }
+    }
+}
