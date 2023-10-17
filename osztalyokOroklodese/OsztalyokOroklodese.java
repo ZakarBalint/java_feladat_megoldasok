8950bb9ddb4caaf6394e7e9451c0683e06d2320e
ZakarBalint <szt2zakbal@vasvari.org>
2023-10-17 13:27:31 +0200

    a

---
+++ osztalyokOroklodese/OsztalyokOroklodese.java
@@ -0,0 +1,26 @@
+package osztalyokOroklodese;
+
+abstract class Animal
+{
+    String name;
+    int age;
+    String gender;
+    
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public void breed()
+    {
+
+    }
+}
+
+public class OsztalyokOroklodese
+{
+    public static void main(String[] args) {
+        
+    }
+}
